f66b543a5c482a13d194a00e3d5f4b595b36832c
david <david.89.mora@gmail.com>
2021-03-05 14:57:18 -0600

    corrección de dependencia

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
-import VueAxios from 'vue-axios'
 import AxiosPlugin from 'vue-axios-cors';
 
-Vue.use(VueAxios, axios, AxiosPlugin)
+Vue.use(axios, AxiosPlugin)
 
 Vue.config.productionTip = false
 
