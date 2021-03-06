8126b0661b2b8417bc27d81d42621972863ff502
david <david.89.mora@gmail.com>
2021-03-06 10:17:37 -0600

    cors

--- src/components/FomularioSymbol.vue
+++ src/components/FomularioSymbol.vue
@@ -103,7 +103,7 @@ export default {
           //axios.get('https://www.findata.co.nz/markets/NYSE/symbols/'+this.symbol.charAt(0)+'.htm', {headers:{}})
           //.then(response => {if (response.data.includes('>'+ this.symbol + '</A></td>')){alert('El simbolo es correcto')} else{alert('Simbolo no valido'); return 0}
           axios.post('https://appnyse.herokuapp.com/api/empresas/', {nombre:this.nombre, descripcion:this.descripcion, simbolo:this.symbol, valores_de_mercado:this.valores})
-          .then(() => {alert('registrado')
+          .then(response => {alert(response.data)})
           this.getEmpresas()
           this.symbol = ''
           this.nombre = '',
@@ -111,7 +111,7 @@ export default {
           this.descripcion = ''
           this.valores = []
           })
-          .catch(error => alert(error))
+          .catch(error => alert(error, 'verificar campos'))
           
           
           
